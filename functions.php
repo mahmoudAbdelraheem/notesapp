7e75288f6ec2222f5f128a8216e6878310f181bf
mahmoudAbdelraheem <mmoud2031@gmail.com>
2024-01-06 17:49:58 +0200

    new update

--- functions.php
+++ functions.php
@@ -52,7 +52,7 @@ function deleteImage($dir , $imageName){
 function checkAuthenticate(){
     if (isset($_SERVER['PHP_AUTH_USER'])  && isset($_SERVER['PHP_AUTH_PW'])) {
 
-        if ($_SERVER['PHP_AUTH_USER'] != "memo" ||  $_SERVER['PHP_AUTH_PW'] != "mmoud2031@"){
+        if ($_SERVER['PHP_AUTH_USER'] != "memo" ||  $_SERVER['PHP_AUTH_PW'] != "mahmoud"){
             header('WWW-Authenticate: Basic realm="My Realm"');
             header('HTTP/1.0 401 Unauthorized');
             echo 'Page Not Found';
